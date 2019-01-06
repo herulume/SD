e4a976055fe06b37f10c1a7bc412dfc39f914846
Mendess2526 <pedro.mendes.26@gmail.com>
2019-01-06 11:20:24 +0000

    Add buy drop test

--- src/test/java/server/ServerTest.java
+++ src/test/java/server/ServerTest.java
@@ -5,6 +5,9 @@ import util.AtomicInt;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ServerTest implements Runnable {
 
@@ -13,15 +16,16 @@ public class ServerTest implements Runnable {
     private final int id;
     private BufferedReader reader;
     private PrintWriter writer;
+    private boolean auctioning;
 
     public static void main(String[] args) {
         final int num_threads = 1000;
         Thread[] threads = new Thread[num_threads];
         for (int i = 1; i < num_threads; i++) {
-            threads[i] = new Thread(new ServerTest());
+            threads[i] = new Thread(new ServerTest(i % 2 == 0));
             threads[i].start();
         }
-        new ServerTest().run();
+        new ServerTest(true).run();
         for (int i = 1; i < num_threads; i++) {
             try {
                 threads[i].join();
@@ -31,8 +35,9 @@ public class ServerTest implements Runnable {
         }
     }
 
-    private ServerTest() {
+    private ServerTest(boolean auctioning) {
         this.id = ServerTest.ID.fetchAndApply(x -> x + 1);
+        this.auctioning = auctioning;
         try {
             Socket s = new Socket("localhost", 5000);
             this.reader = new BufferedReader(new InputStreamReader(s.getInputStream()));
@@ -44,13 +49,21 @@ public class ServerTest implements Runnable {
 
     @Override
     public void run() {
+        if (this.auctioning) {
+            auction();
+        } else {
+            buyAndDrop();
+        }
+    }
+
+    private void auction() {
         String response;
         String message;
         try {
             message = String.format("register %d %d %d", this.id, this.id, this.id);
             this.writer.println(message);
             response = this.reader.readLine();
-            Assert.assertEquals(response, "Registered successfully");
+            Assert.assertTrue(response.equals("Email already registered") || response.equals("Registered successfully"));
             message = String.format("login %d %d", this.id, this.id);
             this.writer.println(message);
             response = this.reader.readLine();
@@ -58,18 +71,69 @@ public class ServerTest implements Runnable {
             message = String.format("auction %d ts1.lower", this.id % 30);
             this.writer.println(message);
             response = this.reader.readLine();
-            Assert.assertTrue(this.toString() + " " + message + " " + response,
+            Assert.assertTrue(this.toString() + " | " + message + " | " + response,
                     response.equals("Auction started!")
-                    || response.equals("Bid added to auction!")
-                    || response.equals("Request queued!")
-                    || response.contains("Please bid higher then the current highest bid:"));
+                            || response.equals("Bid added to auction!")
+                            || response.equals("Request queued!")
+                            || response.contains("Please bid higher then the current highest bid:")
+                            || response.contains("New message"));
+            if (response.contains("Please bid higher")) {
+                int bid = (int) Float.parseFloat(response.substring("Please bid higher then the current highest bid:".length()).trim());
+                message = String.format("auction %d ts1.lower", bid + 1);
+                this.writer.println(message);
+                response = this.reader.readLine();
+                Assert.assertTrue(this.toString() + " | " + message + " | " + response,
+                        response.equals("Bid added to auction!")
+                                || response.contains("Please bid higher then the current highest bid:")
+                                || response.contains("New message"));
+            }
         } catch (IOException e) {
-            Assert.fail();
+            Assert.fail(e.getMessage());
+        }
+    }
+
+    private void buyAndDrop() {
+        String response;
+        String message;
+        try {
+            message = String.format("register %d %d %d", this.id, this.id, this.id);
+            this.writer.println(message);
+            response = this.reader.readLine();
+            Assert.assertTrue(response.equals("Email already registered") || response.equals("Registered successfully"));
+            message = String.format("login %d %d", this.id, this.id);
+            this.writer.println(message);
+            response = this.reader.readLine();
+            Assert.assertEquals(response, "Logged in");
+            message = "buy ts2.high";
+            this.writer.println(message);
+            response = this.reader.readLine();
+            Assert.assertTrue(response, response.contains("Purchase successful!") || response.equals("No stock for type: ts2.high"));
+            message = "ls -m";
+            this.writer.println(message);
+            response = this.reader.readLine();
+            Assert.assertTrue(response.contains("DROPLETS:"));
+            if(!response.contains("You have no droplets")){
+                response = this.reader.readLine();
+                Assert.assertTrue(response, response.contains("ID") && response.contains("NAME"));
+                response = this.reader.readLine();
+                Assert.assertTrue(response, response.contains("====="));
+                response = this.reader.readLine();
+                Assert.assertTrue(response.contains("ts2.high"));
+                List<String> droplet = Arrays.stream(response.split("\t")).filter(s -> !s.isEmpty()).collect(Collectors.toList());
+                this.reader.readLine();
+                this.reader.readLine();
+                message = String.format("drop %d", Integer.parseInt(droplet.get(0)));
+                this.writer.println(message);
+                response = this.reader.readLine();
+                Assert.assertEquals("Server " + Integer.parseInt(droplet.get(0)) + " dropped!", response);
+            }
+        } catch (IOException e) {
+            Assert.fail(e.getMessage());
         }
     }
 
     @Override
     public String toString() {
-        return this.id + "";
+        return "id: " + this.id;
     }
 }
