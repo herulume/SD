387a1913359a397a737e245baa61ad3dc52c3708
Mendess2526 <pedro.mendes.26@gmail.com>
2019-01-06 17:35:19 +0000

    Add prints to Server

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -5,6 +5,7 @@ import server.middleware.Session;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.time.LocalDateTime;
 
 public class Server {
 
@@ -14,8 +15,10 @@ public class Server {
         ServerSocket server = new ServerSocket(port);
         AuctionHouse auctionHouse = new AuctionHouse();
 
+        System.out.println("Server started");
         while (true) {
             Socket clientSocket = server.accept();
+            System.out.println(LocalDateTime.now() + " : Connection accepted");
             new Thread(new Session(clientSocket, auctionHouse)).start();
         }
     }
