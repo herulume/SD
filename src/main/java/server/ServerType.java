9ce09493a5357894644c9fbbfcb9d583157f0302
Mendess2526 <pedro.mendes.26@gmail.com>
2019-01-06 17:00:20 +0000

    Add server types

--- src/main/java/server/ServerType.java
+++ src/main/java/server/ServerType.java
@@ -3,7 +3,10 @@ package server;
 import java.util.Optional;
 
 public enum ServerType {
-    I(12.3f, "ts1.lower"), B(203.3f, "ts2.high");
+    N(12.3f, "ts1.lower"),
+    O(203.3f, "ts2.high"),
+    C(107.8f, "ts3.mid"),
+    A(600.9f, "ts4.sd");
 
     private final float price;
     private final String name;
@@ -24,9 +27,13 @@ public enum ServerType {
     public static Optional<ServerType> fromString(String st) {
         switch (st.toLowerCase()) {
             case "ts1.lower":
-                return Optional.of(ServerType.I);
+                return Optional.of(ServerType.N);
             case "ts2.high":
-                return Optional.of(ServerType.B);
+                return Optional.of(ServerType.O);
+            case "ts3.mid":
+                return Optional.of(ServerType.C);
+            case "ts4.sd":
+                return Optional.of(ServerType.A);
             default:
                 return Optional.empty();
         }
